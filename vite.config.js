c3aaabe32454cf91e14b62559bac65d7a0a4eb93
John Doe <hayotsultonov2005@mail.ru>
2024-01-10 01:41:18 +0500

    save all changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import reactRefresh from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+ build: {
+     outDir: 'build',
+ },
+ plugins: [
+     reactRefresh(),
+     svgrPlugin({
+         svgrOptions: {
+             icon: true,
+         },
+     }),
+ ],
+
 })
